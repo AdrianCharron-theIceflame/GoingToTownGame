e81c6176bb8aa217e10e1d5a00a4c97fb2740b66
Adrian <adriancharron@yahoo.ca>
2024-04-20 14:56:59 -0400

    Added field validation for numRounds

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -32,7 +32,32 @@ window.onload = displayInfo;
 // create a game
 const game = createGame();
 // start the game
-btnStart.addEventListener(`click`, playGame);
+btnStart.setAttribute(`disabled`, ``);
+let span = node(`span`);
+if (fldNumRounds.value >= 1 && fldNumRounds.value <= 11) {
+    span.textContent = '';
+    btnStart.removeAttribute(`disabled`);
+    btnStart.addEventListener(`click`, playGame);
+}
+else {
+    btnStart.setAttribute(`disabled`, ``);
+    span.textContent = `** Number of rounds must be between 1 and 11`;
+    let setUp = $$(`#setUp`);
+    setUp.appendChild(span);
+}
+fldNumRounds.addEventListener(`blur`, () => {
+    if (fldNumRounds.value >= 1 && fldNumRounds.value <= 11) {
+        span.textContent = '';
+        btnStart.removeAttribute(`disabled`);
+        btnStart.addEventListener(`click`, playGame);
+    }
+    else {
+        btnStart.setAttribute(`disabled`, ``);
+        span.textContent = `** Number of rounds must be between 1 and 11`;
+        let setUp = $$(`#setUp`);
+        setUp.appendChild(span);
+    }
+})
 
 // create dice rollers
 const playerDice = new DiceRolls(game.player1);
